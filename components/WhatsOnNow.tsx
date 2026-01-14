4531449636fcdce59925f20f9283240aecbf0a9e
explorier <explorier@users.noreply.github.com>
2026-01-13 23:16:49 -0600

    Remove min-height for quick play skeleton loader

--- components/WhatsOnNow.tsx
+++ components/WhatsOnNow.tsx
@@ -356,7 +356,7 @@ export default function WhatsOnNow() {
           {/* Content */}
           <div className="overflow-y-auto flex-1 overscroll-contain">
             {isLoading && stationsWithData.length === 0 ? (
-              <div className="p-2 space-y-1 min-h-[280px]">
+              <div className={`p-2 space-y-1 ${quickPlayBatchSize ? '' : 'min-h-[280px]'}`}>
                 {[...Array(quickPlayBatchSize || INITIAL_BATCH_SIZE)].map((_, i) => (
                   <div
                     key={i}
